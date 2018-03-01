19989a413245fd7b0efe0f5ce7379b705b266d9c
OlyaZasimovich <olechka-zosya@mail.ru>
2018-03-01 16:17:21 +0300

    quadratic equation first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  equation = equation.replace(/ /g, "");
+
+  var a, b, c, D;
+  var solutions = [];
+  a = equation.match( /([-]?\d+)\*x\^2/i );
+  b = equation.match( /([-]?\d+)\*x(?!\^)/i );
+  c = equation.match( /[-]?\d+\*x\^2[+-]\d+\*x(?!\^)([+-]\d+)/i );
+
+  D = b[1]*b[1] - 4*a[1]*c[1];
+
+  if (D < 0) return "Нет корней";
+
+  if (D > 0) {
+  	x1 = Math.round( ( -b[1] + Math.sqrt(D) ) / (2 * a[1]) );
+  	x2 = Math.round( ( -b[1] - Math.sqrt(D) ) / (2 * a[1]) );
+
+  	if (x1 < x2) {
+  		solutions = [x1, x2];
+  	} else {
+  		solutions = [x2, x1];
+  	}
+  	return solutions;
+  }
 }
